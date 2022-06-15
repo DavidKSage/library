a6fd97fe391cc580cc95661f7b7277bc519e213d
dkstenor <dkstenor@mac.com>
2022-06-15 00:24:05 -0400

    Added ISBN component to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import GetBook from './components/GetBook';
+
 
 function App() {
   return (
     <div className="App">
-      <GetBook />
+      <GetBook isbn_data="9780061997228"/>
     </div>
   );
 }
--- frontend/src/components/GetBook.js
+++ frontend/src/components/GetBook.js
@@ -1,9 +1,25 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const GetBook = () => {
+const GetBook = ({isbn_data}) => {
+
+    const [book, setBook] = useState({
+        author: "",
+        title: ""
+    })
+
+    let isbn = isbn_data;
 
     useEffect(() => {
-        axios.get()
-    }, []);
+        axios.get(`http://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`)
+        .then((response) => {
+            setBook({
+                author: response.data[`ISBN:${isbn}`].details.by_statement,
+                title: response.data[`ISBN:${isbn}`].details.title
+        })
+    })}, [isbn]);
+
+    console.log(book);
 }
+
+export default GetBook;
